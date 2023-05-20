38a7fdb55ec8476310b3c38f3cc80e8b53d092c4
d.baranov <d.baranov@zenit.ru>
2023-05-20 21:58:55 +0300

    task 2

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/ansakharov/mini_course_code
 
-go 1.20
+go 1.18
 
 require golang.org/x/sync v0.2.0
--- tasks/structs_interfaces/src/io_reader.go
+++ tasks/structs_interfaces/src/io_reader.go
@@ -1,7 +1,9 @@
 package src
 
 import (
+	"bytes"
 	"io"
+	"strings"
 )
 
 type Reader interface {
@@ -16,7 +18,39 @@ type CountingToLowerReaderImpl struct {
 }
 
 func (cr *CountingToLowerReaderImpl) Read(p []byte) (int, error) {
-	return 0, nil
+	i, err := cr.Reader.Read(p)
+	if err != nil && err != io.EOF {
+		return i, err
+	}
+
+	cr.TotalBytesRead += int64(i)
+	copy(p, bytes.ToLower(p))
+
+	return i, err
+}
+
+func (cr *CountingToLowerReaderImpl) ReadAll(bufSize int) (string, error) {
+	var err error
+	var ir int
+	ss := make([]byte, 0, bufSize)
+	for {
+		s := make([]byte, bufSize)
+		if ir, err = cr.Read(s); err != nil && err != io.EOF {
+			return "", err
+		}
+		s = s[:ir]
+
+		ss = append(ss, s...)
+		if err != nil {
+			break
+		}
+	}
+
+	return strings.TrimSpace(string(ss)), nil
+}
+
+func (cr *CountingToLowerReaderImpl) BytesRead() int64 {
+	return cr.TotalBytesRead
 }
 
 func NewCountingReader(r io.Reader) *CountingToLowerReaderImpl {
